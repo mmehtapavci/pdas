7318a73066099aceb6b42b52bd2d45525f5904db
mmehtapavci <mmmehtapavci@gmail.com>
2024-01-24 17:53:36 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ProfileCard from './ProfileCard';
+
+function App() {
+    return (
+        <div>
+            <div>Personal Digital Assistans</div>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </div>
+    )
+}
+export default App;
